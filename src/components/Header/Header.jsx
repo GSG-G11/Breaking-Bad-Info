ca936f606ec83e614ba1937b69bd6ca3ae9a288f
khaled-samy <khaled.s.elkhudary@gmail.com>
2022-04-13 23:02:38 +0300

    final styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
     return (
         <>
         <div className='header-container'>
+        <Link to='/' >
             <img src={logo} alt='logo'  />
+            </Link>
             <div className='header-links'>
             <Link to='/' className='btn'>Home </Link>
             <Link to='/characters' className='btn'>Characters </Link>
