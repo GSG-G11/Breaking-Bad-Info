dc7eda3963b30eb22582643a7bca9698bbf53bdb
khaled-samy <khaled.s.elkhudary@gmail.com>
2022-04-13 18:30:33 +0300

    edit home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import characters from '../../assets/characters.webp'
+import episodes from '../../assets/episods.jpg'
 
 const Home = () => {
     return (
         <>
             <h1>Welcome to Breaking Bad information app</h1>
-           <Link to='/characters'>characters</Link>
+            <p>Premise. Set in Albuquerque, New Mexico, 
+                between 2008 and 2010, Breaking Bad follows
+                 Walter White, a meek high school chemistry 
+                 teacher who transforms into a ruthless player
+                  in the local methamphetamine drug trade, 
+                  driven by a desire to financially provide
+                   for his family after being diagnosed with 
+                   terminal lung cancer.</p>
+                   <p>You want to know more?</p>
+                   <div className='char-info'>
+                        <img src={characters} alt="characters" />
+                        <p>This is the list of characters from Breaking Bad series which was shown on AMC</p>
+                     <Link to='/characters'>more</Link>
+                   </div>
            <br></br>
-            <Link to='/episodes'>episodes</Link>
+           <div className='char-info'>
+                        <img src={episodes} alt="episodes" />
+                        <p>It contains 62 episodes divided into 5 seasons</p>
+                     <Link to='/episodes'>more</Link>
+                   </div>
            
         </>
     );
